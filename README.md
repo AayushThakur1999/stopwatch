6a421b967473c3c161cf58dbf69ea9b9e42b3276
AayushThakur1999 <ashcold21@gmail.com>
2022-10-05 23:41:16 +0530

    Upload the stopwatch project first time Update README.md Add index.html, style.css and index.js

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# stopwatch
+# Stopwatch
 A stopwatch made using HTML, CSS and JavaScript.
+
+# HTML
+Used simple div and button tags. Add unique ID's and classes to them.
+
+# CSS
+Used simple styling properties like border, border-radius, cursor, font-family and so on.
+
+# JavaScript
+Used querySelector, addEventListener, textContent etc. methods of the document object.
+Used setInterval, clearInterval, Date.now() etc. methods.
---
+++ index.js
@@ -0,0 +1,69 @@
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let intervalId;
+let startTime = 0;
+let elapsedTime = 0;
+let paused = true;
+let hrs = 0;
+let mins = 0; 
+let secs = 0;
+let millis = 0;
+
+startBtn.addEventListener("click", () => {
+    if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 1);
+    }
+});
+
+pauseBtn.addEventListener("click", () => {
+    if(!paused){
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    millis = 0;
+    timeDisplay.textContent = "00:00:00:000"
+});
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime;
+    millis = elapsedTime % 1000;
+    secs = Math.round(elapsedTime/1000) % 60;
+    mins = Math.round(elapsedTime/(1000 * 60)) % 60;
+    hrs = Math.round(elapsedTime/(1000 * 60 * 60));
+
+    millis = padMillis(millis);
+    hrs = pad(hrs);
+    mins = pad(mins);
+    secs = pad(secs);
+
+    timeDisplay.textContent = `:${hrs}:${mins}:${secs}:${millis}`;
+    
+    function pad(unit){
+        return ("0" + unit).length > 2 ? unit : ("0" + unit);
+    }
+
+    function padMillis(unit){
+        if(("" + unit).length == 1) 
+            return "00" + unit;
+        else if(("" + unit).length == 2) 
+            return "0" + unit;
+        else 
+            return unit;
+    }
+}
